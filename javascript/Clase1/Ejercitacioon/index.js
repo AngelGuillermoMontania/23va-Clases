f85c813199484c38fd1176e5b2f62047d776f8a8
Angel Guillermo Montania <AngelGuillermoMontania@gmail.com>
2024-11-01 22:55:56 -0300

    1clase javascript

---
+++ javascript/Clase1/Ejercitacioon/index.js
@@ -0,0 +1,20 @@
+// Prompt
+
+// let edad
+
+// edad = prompt("Hola, escriba su edad")
+// console.log(edad)
+
+
+// Alert
+
+// alert("HAGA CLICK ACA!!!!")
+
+
+
+
+// EJERCITACION 1
+let nombre = prompt("ESCRIBA SU NOMBRE")
+let apellido = prompt("ESCRIBA SU APELLIDO")
+
+alert("Hola " + nombre + " " + apellido + ", bienvenida a Ada")

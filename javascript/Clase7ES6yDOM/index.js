4447f5df3219cdcb062e1f28faa80804701e7b94
Angel Guillermo Montania <AngelGuillermoMontania@gmail.com>
2024-11-15 20:50:01 -0300

    :safety_vest: clase 6

---
+++ javascript/Clase7ES6yDOM/index.js
@@ -0,0 +1,51 @@
+// SCOPE
+// HOISTING
+// CONTEXTO DE EJECUCION
+
+
+
+
+// const aux = saludar()
+
+// function saludar() {
+//   return "Hola Como va?"
+// }
+
+
+// function sumar(num1, num2) {
+//   return num1 + num2
+// }
+
+// let aux1 = sumar(2,3)  // 5
+// aux1 += 1
+
+// let aux2 = sumar(5,5)
+
+// console.log(aux1) // 6
+// console.log(aux2) // 10
+
+// function sumar(num1, num2) {
+//   return num1 + num2
+// }
+
+// let cosa = sumar(2,3)
+// cosa += 1
+
+// console.log(cosa) // 6
+
+// aux = sumar(6,6) 
+
+// console.log(aux)  // 12
+
+// const edad = 25
+
+// function sumar() {
+//   let resultado = 1
+//   if(edad >= 18) {
+//     resultado + 1
+//   }
+//   return resultado  
+// }
+
+// sumar() // 2    // 1
+// sumar() // 3    // 1

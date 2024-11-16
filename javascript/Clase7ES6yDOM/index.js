6acfddb017e71cd4612e9cd230461efa37006cb5
Angel Guillermo Montania <AngelGuillermoMontania@gmail.com>
2024-11-15 21:52:03 -0300

    clase 7

--- javascript/Clase7ES6yDOM/index.js
+++ javascript/Clase7ES6yDOM/index.js
@@ -49,3 +49,70 @@
 
 // sumar() // 2    // 1
 // sumar() // 3    // 1
+
+
+/* -------------------- CAJERO ----------------------------- */
+
+// SALDO DISPONIBLE DEL USUARIO
+let saldoDisponible = 50000;
+
+// LA CONSTRASEÑA QUE ESTA GUARDADA
+const pinGuardado = "1234";
+
+// Si el usuario escribio bien su contraseña continuar el proceso, si no, avisar que no puede
+// continuar
+
+//  FUNCION QUE HABILITA AL USUARIO A OPERAR
+function autenticar() {
+  const pinIngresado = prompt("Ingrese su pin")
+
+  return pinGuardado === pinIngresado
+}
+
+function retirarDinero() {
+  const montoIngresado = prompt("Ingrese el dinero a retirar")
+
+  if(Number(montoIngresado) <= saldoDisponible) {
+    saldoDisponible -= Number(montoIngresado)
+  } else {
+    alert("Saldo insuficiente")
+  }
+}
+
+function agregarDinero() {
+  const montoIngresado = prompt("Ingrese el dinero a agregar")
+
+  if(montoIngresado > 0) {
+    saldoDisponible += Number(montoIngresado)
+  } else {
+    alert("Operacion no valida")
+  }
+}
+
+function consultarSaldo() {
+  alert(`Su saldo es ${saldoDisponible}`)
+}
+
+if(autenticar()) {
+  const consulta = prompt(`Que quieres hacer? Ingrese solo el numero 
+    1.Retirar Dinero 
+    2.Agregar Dinero
+    3.Consultar Saldo`
+  )
+  if (consulta === "1") {
+    // Retirar Dinero
+    retirarDinero()
+    consultarSaldo()
+  } else if (consulta === "2") {
+    // Agregar dinero
+    agregarDinero()
+    consultarSaldo()
+  } else if (consulta === "3") {
+    // Consultar saldo
+    consultarSaldo()
+  } else {
+    alert("No es una opcion disponible")
+  }
+} else {
+  alert("No se pudo autenticar")
+}

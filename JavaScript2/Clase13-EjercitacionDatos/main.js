ba816aef75475c53c901a88673289f36a020fa6a
Angel Guillermo Montania <AngelGuillermoMontania@gmail.com>
2025-01-30 20:57:46 -0300

    ejercitacion

---
+++ JavaScript2/Clase13-EjercitacionDatos/main.js
@@ -0,0 +1,10 @@
+import datos from "./datos.js";
+
+function $(selector) {
+  return document.querySelector(selector);
+}
+function $$(selector) {
+  return document.querySelectorAll(selector);
+}
+
+/* Al seleccionar la opcion correspondiente, filtrar por su genero. Hacer lo mismo con species (Human y no human) y casa */
